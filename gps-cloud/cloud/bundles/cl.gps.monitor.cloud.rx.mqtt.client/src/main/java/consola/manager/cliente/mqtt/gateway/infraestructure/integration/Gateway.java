b12f7ec9221cf93bd0e2a0f3dc1891c36f38ec6c
developer-agricola <39532120+developer-agricola@users.noreply.github.com>
2019-04-20 14:23:26 -0400

    remove mqtt client
    remove mqtt client

--- gps-cloud/cloud/bundles/cl.gps.monitor.cloud.rx.mqtt.client/src/main/java/consola/manager/cliente/mqtt/gateway/infraestructure/integration/Gateway.java
+++
@@ -1,8 +0,0 @@
-package consola.manager.cliente.mqtt.gateway.infraestructure.integration;
-
-/**
- * Created by daniel.carvajal on 20-06-2018.
- */
-public interface Gateway {
-
-}
--- gps-cloud/cloud/bundles/cl.gps.monitor.cloud.rx.mqtt.client/src/main/java/consola/manager/cliente/mqtt/gateway/infraestructure/subscriber/MessagePublisher.java
+++
@@ -1,9 +0,0 @@
-package consola.manager.cliente.mqtt.gateway.infraestructure.subscriber;
-
-import java.util.function.Function;
-
-/**
- * Created by daniel.carvajal on 29-03-2019.
- */
-public interface MessagePublisher<T, R> extends Function<T, R> {
-}
