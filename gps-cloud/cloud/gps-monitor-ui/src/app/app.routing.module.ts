a37b6e37668abd2bc4592b41e2d73d31a3cc71dd
Daniel Carvajal Soto <dcarvajals.imagemaker@bancochile.cl>
2020-10-27 11:34:33 -0300

    - Se realiza la primera iteracion de la ui

---
+++ gps-cloud/cloud/gps-monitor-ui/src/app/app.routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
